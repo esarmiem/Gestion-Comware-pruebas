2af0ebffa11a857b346834be2d21b24347c4189b
Brams <brayan.msanmartin@gmail.com>
2023-03-24 18:59:57 -0500

    en el archivo text 1.Actualizacion estan los cmabios que hice

--- styles-scripts-pruebas/script-apertura-doble-2cliente.js
+++ styles-scripts-pruebas/script-apertura-doble-2cliente.js
@@ -1,108 +1,50 @@
 // CLEAR BUTTON
-
-function clearTextAreaD() {
+let clearTextAreaD =()=> {
+    let outputs = document.querySelectorAll('[id^="output-data-"], [id^="output-dataB"]');
+    for (let i = 0; i < outputs.length; i++) {
+      outputs[i].value = "";
+    };
     document.getElementById("input-dataD").value = "";
-    document.getElementById('output-data-1-d').value = "";
-    document.getElementById('output-data-2-d').value = "";
-    document.getElementById('output-data-3-d').value = "";
-    document.getElementById('output-data-4-d').value = "";
-    document.getElementById('output-data-5-d').value = "";
-    document.getElementById('output-data-6-d').value = "";
-    document.getElementById('output-data-7-d').value = "";
-    document.getElementById('output-data-8-d').value = "";
-    document.getElementById('output-data-9-d').value = "";
-    // 
     document.getElementById("input-dataD-1").value = "";
-    document.getElementById('output-data-1-1-d').value = "";
-    document.getElementById('output-data-2-1-d').value = "";
-    document.getElementById('output-data-3-1-d').value = "";
-    document.getElementById('output-data-4-1-d').value = "";
-    document.getElementById('output-data-5-1-d').value = "";
-    document.getElementById('output-data-6-1-d').value = "";
-    document.getElementById('output-data-7-1-d').value = "";
-    document.getElementById('output-area-resumenD').value = "";
-    document.getElementById('output-areaD').value = "";
-}
-
+    document.getElementById("output-area-resumenD").value = "";
+    document.getElementById("output-areaD").value = "";
+  };
 // CLEAR BUTTON
 
 // INSERTS DATA FROM A TEXTAREA INPUT AND FILLS OTHER INPUTS
 // CLIENTE EXTREMO A
 const inputDataD = document.getElementById('input-dataD');
-const outputData1D = document.getElementById('output-data-1-d');
-const outputData2D = document.getElementById('output-data-2-d');
-const outputData3D = document.getElementById('output-data-3-d');
-const outputData4D = document.getElementById('output-data-4-d');
-const outputData5D = document.getElementById('output-data-5-d');
-const outputData6D = document.getElementById('output-data-6-d');
-const outputData7D = document.getElementById('output-data-7-d');
-const outputData8D = document.getElementById('output-data-8-d');
-const outputData9D = document.getElementById('output-data-9-d');
+const outputData = document.querySelectorAll('[id^="output-data-"]');
 
 inputDataD.addEventListener('input', () => {
     const dataD = inputDataD.value.split('\n');
-    outputData1D.value = dataD[0] || '';
-    outputData2D.value = dataD[1] || '';
-    outputData3D.value = dataD[2] || '';
-    outputData4D.value = dataD[3] || '';
-    outputData5D.value = dataD[4] || '';
-    outputData6D.value = dataD[5] || '';
-    outputData7D.value = dataD[6] || '';
-    outputData8D.value = dataD[7] || '';
-    outputData9D.value = dataD[8] || '';
+    outputData.forEach((output, index) => {
+        output.value = dataD[index] || '';
+    });
 });
-// INSERTS DATA FROM A TEXTAREA INPUT AND FILLS OTHER INPUTS
-
-// INSERTS DATA FROM A TEXTAREA INPUT AND FILLS OTHER INPUTS
 // CLIENTE EXTREMO B
 const inputDataD1 = document.getElementById('input-dataD-1');
-const outputData1D1 = document.getElementById('output-data-1-1-d');
-const outputData2D1 = document.getElementById('output-data-2-1-d');
-const outputData3D1 = document.getElementById('output-data-3-1-d');
-const outputData4D1 = document.getElementById('output-data-4-1-d');
-const outputData5D1 = document.getElementById('output-data-5-1-d');
-const outputData6D1 = document.getElementById('output-data-6-1-d');
-const outputData7D1 = document.getElementById('output-data-7-1-d');
+const outputData1 = document.querySelectorAll('[id^="output-dataB-"]');
 
 inputDataD1.addEventListener('input', () => {
     const dataD1 = inputDataD1.value.split('\n');
-    outputData1D1.value = dataD1[0] || '';
-    outputData2D1.value = dataD1[1] || '';
-    outputData3D1.value = dataD1[2] || '';
-    outputData4D1.value = dataD1[3] || '';
-    outputData5D1.value = dataD1[4] || '';
-    outputData6D1.value = dataD1[5] || '';
-    outputData7D1.value = dataD1[6] || '';
+    outputData1.forEach((output, index) => {
+        output.value = dataD1[index] || '';
+    });
 });
 // INSERTS DATA FROM A TEXTAREA INPUT AND FILLS OTHER INPUTS
-// 
-// 
+
 // FUNCION CHECK IF SI OR NO ARE CHECK AND UNCHECK THE OTHER
 // CLIENTE EXTERMO A
-function uncheckNoD() {
-    document.getElementById("no-retiroD").checked = false;
-}
-
-function uncheckSiD() {
-    document.getElementById("si-retiroD").checked = false;
-}
-// FUNCION CHECK IF SI OR NO ARE CHECK AND UNCHECK THE OTHER
-
-// FUNCION CHECK IF SI OR NO ARE CHECK AND UNCHECK THE OTHER
+let uncheckNoD =()=> { document.getElementById("no-retiroD").checked = false;};
+let uncheckSiD =()=> { document.getElementById("si-retiroD").checked = false;};
 // CLIENTE EXTREMO B
-function uncheckNoD1() {
-    document.getElementById("no-retiroD-1").checked = false;
-}
-
-function uncheckSiD1() {
-    document.getElementById("si-retiroD-1").checked = false;
-}
+let uncheckNoD1 =()=> { document.getElementById("no-retiroD-1").checked = false;};
+let uncheckSiD1 =()=> { document.getElementById("si-retiroD-1").checked = false;};
 // FUNCION CHECK IF SI OR NO ARE CHECK AND UNCHECK THE OTHER
 
 // FUNCION PARA AGREGAR LA INFORMACION RESPECTIVA
-
-function generateOutputD() {
-    const outputD = [];
+let generateOutputD =()=> {
     const disponibilidadD = document.getElementById("disponibilidadD").value;
     const disponibilidadD2 = document.getElementById("disponibilidad-2D").value;
     const disponibilidadD1 = document.getElementById("disponibilidadD-1").value;
@@ -110,45 +52,42 @@ function generateOutputD() {
     const pedidoRetiroD = document.querySelector('input[name="retiroD-1"]:checked').value;
     const pedidoRetiroD1 = document.querySelector('input[name="retiroD-1"]:checked').value;
 
-    outputD.push('SE PRESENTA APERTURA DOBLE EN EL ANILLO ' + document.getElementById('output-data-1-d').value + '(' + document.getElementById('output-data-2-d').value + ')' + ' ENTRE LOS CLIENTES ' + document.getElementById('output-data-3-d').value + ' Y ' + document.getElementById('output-data-1-1-d').value);
-    outputD.push('ANILLO: ' + document.getElementById('output-data-1-d').value);
-    outputD.push('CIUDAD: ' + document.getElementById('output-data-2-d').value);
-    outputD.push('NOMBRE CLIENTE CAIDO EXTREMO A: ' + document.getElementById('output-data-3-d').value);
-    outputD.push('IP SWITCH CLIENTE EXTREMO A: ' + document.getElementById('output-data-4-d').value);
-    outputD.push('CONTACTO CLIENTE CAIDO EXTREMO A : ' + document.getElementById('output-data-5-d').value);
-    outputD.push('CORREO CLIENTE CAIDO EXTREMO A: ' + document.getElementById('output-data-6-d').value);
-    outputD.push('DIRECCIÓN CLIENTE CAIDO EXTREMO A: ' + document.getElementById('output-data-8-d').value);
-    outputD.push('TELÉFONO CLIENTE CAIDO EXTREMO A: ' + document.getElementById('output-data-7-d').value);
-    outputD.push('DISPONIBILIAD HORARIA CLIENTE EXTREMO A: ' + disponibilidadD + ' a ' + disponibilidadD2);
-    outputD.push('PEDIDO DE RETIRO (SI/NO): ' + pedidoRetiroD);
-    outputD.push('DESCARTES REALIZADOS CLIENTE EXTREMO A: ' + document.getElementById('output-data-9-d').value);
-    outputD.push('\n');
-    outputD.push('NOMBRE CLIENTE CAIDO EXTREMO B: ' + document.getElementById('output-data-1-1-d').value);
-    outputD.push('IP SWITCH CLIENTE EXTREMO B: ' + document.getElementById('output-data-2-1-d').value);
-    outputD.push('CONTACTO CLIENTE CAIDO EXTREMO B: ' + document.getElementById('output-data-3-1-d').value);
-    outputD.push('CORREO CLIENTE CAIDO EXTREMO B: ' + document.getElementById('output-data-4-1-d').value);
-    outputD.push('DIRECCIÓN CLIENTE CAIDO EXTREMO B: ' + document.getElementById('output-data-6-1-d').value);
-    outputD.push('TELÉFONO CLIENTE CAIDO EXTREMO B: ' + document.getElementById('output-data-5-1-d').value);
-    outputD.push('DISPONIBILIAD HORARIA CLIENTE EXTREMO B: ' + disponibilidadD1 + ' a ' + disponibilidadD2D);
-    outputD.push('PEDIDO DE RETIRO (SI/NO): ' + pedidoRetiroD1);
-    outputD.push('DESCARTES REALIZADOS CLIENTE EXTREMO B: ' + document.getElementById('output-data-7-1-d').value);
+    const outputD = `SE PRESENTA APERTURA DOBLE EN EL ANILLO ${document.getElementById('output-data-1-d').value}(${document.getElementById('output-data-2-d').value}) ENTRE LOS CLIENTES ${document.getElementById('output-data-3-d').value} Y ${document.getElementById('output-dataB-1-d').value}
+ANILLO: ${document.getElementById('output-data-1-d').value}
+CIUDAD: ${document.getElementById('output-data-2-d').value}
+NOMBRE DEL CLIENTE CAIDO EXTREMO A: ${document.getElementById('output-data-3-d').value}
+IP SWITCH CLIENTE EXTREMO A: ${document.getElementById('output-data-4-d').value}
+CONTACTO CLIENTE CAIDO EXTREMO A : ${document.getElementById('output-data-5-d').value}
+CORREO CLIENTE CAIDO EXTREMO A: ${document.getElementById('output-data-6-d').value}
+DIRECCIÓN CLIENTE CAIDO EXTREMO A: ${document.getElementById('output-data-8-d').value}
+TELÉFONO CLIENTE CAIDO EXTREMO A: ${document.getElementById('output-data-7-d').value}
+DISPONIBILIAD HORARIA CLIENTE EXTREMO A: ${disponibilidadD} a ${disponibilidadD2}
+PEDIDO DE RETIRO (SI/NO): ${pedidoRetiroD}
+DESCARTES REALIZADOS CLIENTE EXTREMO A: ${document.getElementById('output-data-9-d').value}
+
+NOMBRE CLIENTE CAIDO EXTREMO B: ${document.getElementById('output-dataB-1-d').value}
+IP SWITCH CLIENTE EXTREMO B: ${document.getElementById('output-dataB-2-d').value}
+CONTACTO CLIENTE CAIDO EXTREMO B: ${document.getElementById('output-dataB-3-d').value}
+CORREO CLIENTE CAIDO EXTREMO B: ${document.getElementById('output-dataB-4-d').value}
+DIRECCIÓN CLIENTE CAIDO EXTREMO B: ${document.getElementById('output-dataB-6-d').value}
+TELÉFONO CLIENTE CAIDO EXTREMO B: ${document.getElementById('output-dataB-5-d').value}
+DISPONIBILIAD HORARIA CLIENTE EXTREMO B: ${disponibilidadD1} a ${disponibilidadD2D}
+PEDIDO DE RETIRO (SI/NO): ${pedidoRetiroD1}
+DESCARTES REALIZADOS CLIENTE EXTREMO B: ${document.getElementById('output-dataB-7-d').value}`;
 
     const outputAreaD = document.getElementById('output-areaD');
-    outputAreaD.value = outputD.join('\n');
-}
+    outputAreaD.value = outputD;
+};
 
-function generateOutputResumenD() {
-    const outputD = [];
-
-    outputD.push('SE PRESENTA APERTURA DOBLE EN EL ANILLO ' + document.getElementById('output-data-1-d').value + '(' + document.getElementById('output-data-2-d').value + ')' + ' ENTRE LOS CLIENTES ' + document.getElementById('output-data-3-d').value + ' Y ' + document.getElementById('output-data-1-1-d').value);
-
+let generateOutputResumenD =()=> { 
+    const outputD = `SE PRESENTA APERTURA DOBLE EN EL ANILLO ${document.getElementById('output-data-1-d').value}(${document.getElementById('output-data-2-d').value}) ENTRE LOS CLIENTES ${document.getElementById('output-data-3-d').value} Y ${document.getElementById('output-dataB-1-d').value}`;
     const outputArea_1D = document.getElementById('output-area-resumenD');
-    outputArea_1D.value = outputD.join('\n');
-}
+    outputArea_1D.value = outputD;
+};
 
-function copyToClipboardD(elementId) {
-    var copyText = document.getElementById(elementId);
+let copyToClipboardD =(elementId)=> {
+    let copyText = document.getElementById(elementId);
     copyText.select();
     document.execCommand("copy");
     // alert("Copied to clipboard!");
-}
+};
--- styles-scripts-pruebas/script-apertura-simple.js
+++ styles-scripts-pruebas/script-apertura-simple.js
@@ -1,114 +1,50 @@
 // CLEAR BUTTON
-
-function clearTextAreaA() {
+let clearTextAreaA =()=> {
+    let outputs = document.querySelectorAll('[id^="output-data-"], [id^="output-dataB"]');
+    for (let i = 0; i < outputs.length; i++) {
+      outputs[i].value = "";
+    }
     document.getElementById("input-dataA").value = "";
-    document.getElementById('output-data-1').value = "";
-    document.getElementById('output-data-2').value = "";
-    document.getElementById('output-data-3').value = "";
-    document.getElementById('output-data-4').value = "";
-    document.getElementById('output-data-5').value = "";
-    document.getElementById('output-data-6').value = "";
-    document.getElementById('output-data-7').value = "";
-    document.getElementById('output-data-8').value = "";
-    document.getElementById('output-data-9').value = "";
-    document.getElementById('output-data-10').value = "";
-    // 
     document.getElementById("input-dataB").value = "";
-    document.getElementById('output-data-1-1').value = "";
-    document.getElementById('output-data-2-1').value = "";
-    document.getElementById('output-data-3-1').value = "";
-    document.getElementById('output-data-4-1').value = "";
-    document.getElementById('output-data-5-1').value = "";
-    document.getElementById('output-data-6-1').value = "";
-    document.getElementById('output-data-7-1').value = "";
-    document.getElementById('output-data-8-1').value = "";
-    document.getElementById('output-area-resumen').value = "";
-    document.getElementById('output-area').value = "";
-}
-
+    document.getElementById("output-area-resumen").value = "";
+    document.getElementById("output-area").value = "";
+  };
 // CLEAR BUTTON
 
 // INSERTS DATA FROM A TEXTAREA INPUT AND FILLS OTHER INPUTS
 // CLIENTE EXTREMO A
-const inputDataA = document.getElementById('input-dataA');
-const outputData1 = document.getElementById('output-data-1');
-const outputData2 = document.getElementById('output-data-2');
-const outputData3 = document.getElementById('output-data-3');
-const outputData4 = document.getElementById('output-data-4');
-const outputData5 = document.getElementById('output-data-5');
-const outputData6 = document.getElementById('output-data-6');
-const outputData7 = document.getElementById('output-data-7');
-const outputData8 = document.getElementById('output-data-8');
-const outputData9 = document.getElementById('output-data-9');
-const outputData10 = document.getElementById('output-data-10');
+const inputDataA = document.getElementById("input-dataA");
+const outputData = document.querySelectorAll('[id^="output-data-"]');
 
 inputDataA.addEventListener('input', () => {
     const data = inputDataA.value.split('\n');
-    outputData1.value = data[0] || '';
-    outputData2.value = data[1] || '';
-    outputData3.value = data[2] || '';
-    outputData4.value = data[3] || '';
-    outputData5.value = data[4] || '';
-    outputData6.value = data[5] || '';
-    outputData7.value = data[6] || '';
-    outputData8.value = data[7] || '';
-    outputData9.value = data[8] || '';
-    outputData10.value = data[9] || '';
+    outputData.forEach((output, index) => {
+        output.value = data[index] || '';
+    });
 });
-// INSERTS DATA FROM A TEXTAREA INPUT AND FILLS OTHER INPUTS
-
-// INSERTS DATA FROM A TEXTAREA INPUT AND FILLS OTHER INPUTS
 // CLIENTE EXTREMO B
 const inputDataB = document.getElementById('input-dataB');
-const outputData1B = document.getElementById('output-data-1-1');
-const outputData2B = document.getElementById('output-data-2-1');
-const outputData3B = document.getElementById('output-data-3-1');
-const outputData4B = document.getElementById('output-data-4-1');
-const outputData5B = document.getElementById('output-data-5-1');
-const outputData6B = document.getElementById('output-data-6-1');
-const outputData7B = document.getElementById('output-data-7-1');
-const outputData8B = document.getElementById('output-data-8-1');
+const outputDataB = document.querySelectorAll('[id^="output-dataB-"]');
 
 inputDataB.addEventListener('input', () => {
     const dataB = inputDataB.value.split('\n');
-    outputData1B.value = dataB[0] || '';
-    outputData2B.value = dataB[1] || '';
-    outputData3B.value = dataB[2] || '';
-    outputData4B.value = dataB[3] || '';
-    outputData5B.value = dataB[4] || '';
-    outputData6B.value = dataB[5] || '';
-    outputData7B.value = dataB[6] || '';
-    outputData8B.value = dataB[7] || '';
+    outputDataB.forEach((output, index) => {
+        output.value = dataB[index] || '';
+    });
 });
 // INSERTS DATA FROM A TEXTAREA INPUT AND FILLS OTHER INPUTS
-// 
-// 
+ 
 // FUNCION CHECK IF SI OR NO ARE CHECK AND UNCHECK THE OTHER
 // CLIENTE EXTERMO A
-function uncheckNo() {
-    document.getElementById("no-retiro").checked = false;
-}
-
-function uncheckSi() {
-    document.getElementById("si-retiro").checked = false;
-}
-// FUNCION CHECK IF SI OR NO ARE CHECK AND UNCHECK THE OTHER
-
-// FUNCION CHECK IF SI OR NO ARE CHECK AND UNCHECK THE OTHER
+let uncheckNo =()=> { document.getElementById("no-retiro").checked = false; };
+let uncheckSi =()=> { document.getElementById("si-retiro").checked = false; };
 // CLIENTE EXTREMO B
-function uncheckNoB() {
-    document.getElementById("no-retiroB").checked = false;
-}
-
-function uncheckSiB() {
-    document.getElementById("si-retiroB").checked = false;
-}
+let uncheckNoB =()=> { document.getElementById("no-retiroB").checked = false; };
+let uncheckSiB =()=> { document.getElementById("si-retiroB").checked = false; };
 // FUNCION CHECK IF SI OR NO ARE CHECK AND UNCHECK THE OTHER
 
 // FUNCION PARA AGREGAR LA INFORMACION RESPECTIVA
-
-function generateOutput() {
-    const output = [];
+let generateOutput =()=> {
     const disponibilidad = document.getElementById("disponibilidad").value;
     const disponibilidad2 = document.getElementById("disponibilidad-2").value;
     const disponibilidadB = document.getElementById("disponibilidadB").value;
@@ -118,48 +54,45 @@ function generateOutput() {
     const pedidoRetiro = document.querySelector('input[name="retiro"]:checked').value;
     const pedidoRetiroB = document.querySelector('input[name="retiroB"]:checked').value;
 
-    output.push('Se presenta apertura simple en el ANILLO: ' + document.getElementById('output-data-1').value + ' En la ciudad de: ' + document.getElementById('output-data-2').value + ' Entre los clientes: ' + document.getElementById('output-data-3').value + ' Por el puerto GIGA ' + document.getElementById('output-data-5').value + ' Y el Puerto GIGA: ' + document.getElementById('output-data-3-1').value);
     // output.push('CIUDAD: ' + document.getElementById('output-data-2').value);
-    output.push('NOMBRE CLIENTE EXTREMO A: ' + document.getElementById('output-data-3').value);
-    output.push('IP SWITCH EXTREMO A: ' + document.getElementById('output-data-4').value);
-    output.push('PUERTO SWITCH CAÍDO EN EXTREMO A: ' + document.getElementById('output-data-5').value);
-    output.push('CONTACTO CLIENTE EXTREMO A: ' + document.getElementById('output-data-6').value);
-    output.push('DIRECCIÓN CLIENTE EXTREMO A: ' + document.getElementById('output-data-7').value);
-    output.push('TELÉFONO CLIENTE EXTREMO A: ' + document.getElementById('output-data-8').value);
-    output.push('CORREO CLIENTE EXTREMO A: ' + document.getElementById('output-data-9').value);
-    output.push('DISPONIBILIAD HORARIA CLIENTE EXTREMO A: ' + disponibilidad + ' a ' + disponibilidad2);
-    output.push('MEDIDAS TOMADAS CON OTDR CLIENTE A: ' + medidasTomadas);
-    output.push('PEDIDO DE RETIRO (SI/NO): ' + pedidoRetiro);
-    output.push('DESCARTES REALIZADOS CLIENTE EXTREMO A: ' + document.getElementById('output-data-10').value);
-    output.push('\n');
-    output.push('NOMBRE CLIENTE EXTREMO B: ' + document.getElementById('output-data-1-1').value);
-    output.push('IP SWITCH EXTREMO B: ' + document.getElementById('output-data-2-1').value);
-    output.push('PUERTO SWITCH CAÍDO EN EXTREMO B: ' + document.getElementById('output-data-3-1').value);
-    output.push('CONTACTO CLIENTE EXTREMO B: ' + document.getElementById('output-data-4-1').value);
-    output.push('DIRECCIÓN CLIENTE EXTREMO B: ' + document.getElementById('output-data-5-1').value);
-    output.push('TELÉFONO CLIENTE EXTREMO B: ' + document.getElementById('output-data-6-1').value);
-    output.push('CORREO CLIENTE EXTREMO B: ' + document.getElementById('output-data-7-1').value);
-    output.push('DISPONIBILIAD HORARIA CLIENTE EXTREMO B: ' + disponibilidadB + ' a ' + disponibilidad2B);
-    output.push('MEDIDAS TOMADAS CON OTDR CLIENTE B: ' + medidasTomadasB);
-    output.push('PEDIDO DE RETIRO (SI/NO): ' + pedidoRetiroB);
-    output.push('DESCARTES REALIZADOS CLIENTE EXTREMO B: ' + document.getElementById('output-data-8-1').value);
+    const output = `Se presenta apertura simple en el ANILLO: ${document.getElementById('output-data-1').value} En la ciudad de: ${document.getElementById('output-data-2').value} Entre los clientes: ${document.getElementById('output-data-3').value} Por el puerto GIGA ${document.getElementById('output-data-5').value} Y el Puerto GIGA: ${document.getElementById('output-dataB-3-1').value}
+NOMBRE CLIENTE EXTREMO A: ${document.getElementById('output-data-3').value}
+IP SWITCH EXTREMO A: ${document.getElementById('output-data-4').value}
+PUERTO SWITCH CAÍDO EN EXTREMO A: ${document.getElementById('output-data-5').value}
+CONTACTO CLIENTE EXTREMO A: ${document.getElementById('output-data-6').value}
+DIRECCIÓN CLIENTE EXTREMO A: ${document.getElementById('output-data-7').value}
+TELÉFONO CLIENTE EXTREMO A: ${document.getElementById('output-data-8').value}
+CORREO CLIENTE EXTREMO A: ${document.getElementById('output-data-9').value}
+DISPONIBILIAD HORARIA CLIENTE EXTREMO A: ${disponibilidad} a ${disponibilidad2}
+MEDIDAS TOMADAS CON OTDR CLIENTE A: ${medidasTomadas}
+PEDIDO DE RETIRO (SI/NO): ${pedidoRetiro}
+DESCARTES REALIZADOS CLIENTE EXTREMO A: ${document.getElementById('output-data-10').value}
 
+NOMBRE CLIENTE EXTREMO B: ${document.getElementById('output-dataB-1-1').value}
+IP SWITCH EXTREMO B: ${document.getElementById('output-dataB-2-1').value}
+PUERTO SWITCH CAÍDO EN EXTREMO B: ${document.getElementById('output-dataB-3-1').value}
+CONTACTO CLIENTE EXTREMO B: ${document.getElementById('output-dataB-4-1').value}
+DIRECCIÓN CLIENTE EXTREMO B: ${document.getElementById('output-dataB-5-1').value}
+TELÉFONO CLIENTE EXTREMO B: ${document.getElementById('output-dataB-6-1').value}
+CORREO CLIENTE EXTREMO B: ${document.getElementById('output-dataB-7-1').value}
+DISPONIBILIAD HORARIA CLIENTE EXTREMO B: ${disponibilidadB} a ${disponibilidad2B}
+MEDIDAS TOMADAS CON OTDR CLIENTE B: ${medidasTomadasB}
+PEDIDO DE RETIRO (SI/NO): ${pedidoRetiroB}
+DESCARTES REALIZADOS CLIENTE EXTREMO B: ${document.getElementById('output-dataB-8-1').value}
+`;
     const outputArea = document.getElementById('output-area');
-    outputArea.value = output.join('\n');
-}
+    outputArea.value = output;
+};
 
-function generateOutputResumen() {
-    const output = [];
-
-    output.push('Se presenta apertura simple en el ANILLO: ' + document.getElementById('output-data-1').value + ' En la ciudad de: ' + document.getElementById('output-data-2').value + ' Entre los clientes: ' + document.getElementById('output-data-3').value + ' Por el puerto GIGA ' + document.getElementById('output-data-5').value + ' Y el Puerto GIGA: ' + document.getElementById('output-data-3-1').value);
-
+let generateOutputResumen =()=> {
+    const output = `Se presenta apertura simple en el ANILLO: ${document.getElementById('output-data-1').value} En la ciudad de: ${document.getElementById('output-data-2').value} Entre los clientes: ${document.getElementById('output-data-3').value} Por el puerto GIGA ${document.getElementById('output-data-5').value} Y el Puerto GIGA: ${document.getElementById('output-dataB-3-1').value}`;
     const outputArea_1 = document.getElementById('output-area-resumen');
-    outputArea_1.value = output.join('\n');
-}
+    outputArea_1.value = output;
+};
 
-function copyToClipboard(elementId) {
+let copyToClipboard =(elementId)=> {
     var copyText = document.getElementById(elementId);
     copyText.select();
     document.execCommand("copy");
     // alert("Copied to clipboard!");
-}
+};
